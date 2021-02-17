725ddb679e4f4e0f0f11a77a8db11c8cef084da0
nhashley21 <nicholas.ashley82@gmail.com>
2021-02-17 10:09:34 -0500

    House keeping and updating scripts.

---
+++ C++/cout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!!" << end1;
+	cout << "My name is ZeroKewl..." << end1;
+	cout << "Welcome!!!" << end1;
+	
+	return 0;
+}
---
+++ C++/str-example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string food1 = "pizza"
+	string food2 = "pasta"
+	string food3 = "burger"
+	cout << food1 << food2 << food3 << end1;
+
+	cout << "Today I ate " << food1 << end1;
+	cout << food1 << " is better than " << food2 << " and " << food2 << " is better than " << food3 << end1;
+	
+	return 0;
+}
