b325c21734ef56e5c2104cad941b5342eba26c26
nhashley21 <nicholas.ashley82@gmail.com>
2021-09-10 10:25:42 -0400

    Added checkra1n download script.

---
+++ Bash/checkrain.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'deb https://assets.checkra.in/debian /' | sudo tee /etc/apt/sources.list.d/checkra1n.list
+sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
+sudo apt-get update
+sudo apt-get install checkra1n
