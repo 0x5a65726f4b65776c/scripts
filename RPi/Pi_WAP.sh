8814816b0acb68231d1c38c23312cc7fec2bf28d
0x5a65726f4b65776c <nicholas.ashley82@gmail.com>
2022-03-16 17:51:05 -0400

    Added ssid and passphrase variables
    Also corrected spelling errors.

--- RPi/Pi_WAP.sh
+++ RPi/Pi_WAP.sh
@@ -5,12 +5,15 @@
 {
 echo "Logging script errors to $PWD/Pi_WAP_Error_Log.txt"
 
-echo "[+]...Updating sudo system...[+]"
+read -p 'Network name: ' ssid
+read -p 'Network passphrase: ' passphrase
+
+echo "[+]...Updating your system...[+]"
 sleep 3
-apt update && apt full-upgrade -y && apt autoremove -y && apt autoclean
+apt update && apt full-upgrade -y && apt autoclean && apt autoremove -y
 sleep 1
 
-echo "[+]...Installing TOR, Privoxy, HostAPD, IPTables, bridge utils, and DNSMasq...[+]"
+echo "[+]...Installing TOR, Privoxy, HostAPD, IPTables, Bridge Utils, and DNSMasq...[+]"
 apt install -y tor privoxy hostapd dnsmasq iptables bridge-utils
 
 echo "[+]...Temporarily stopping HostAPD, DNSMasq, TOR, and Privoxy services...[+]"
@@ -22,28 +25,26 @@ sudo systemctl stop tor
 sudo systemctl stop privoxy
 
 
-echo "[+]...Editing DHCP confing finle...[+]"
+echo "[+]...Editing the DHCP config file...[+]"
 sleep 3
 printf "interface wlan0\nstatic ip_address=192.168.0.1/24\ndenyinterfaces eth0\ndenyinterfaces wlan0\n" | tee -a /etc/dhcpcd.conf
 tail /etc/dhcpcd.conf
 sleep 3
 
-echo "[+]...Confinguring DHCP server...[+]"
+echo "[+]...Configuring the DHCP server...[+]"
 sleep 1
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 printf "interface=wlan0\n dhcp-range=192.168.0.2,192.168.0.254,255.255.255.0,24h\n" | tee -a /etc/dnsmasq.conf
 
 
-echo "[+]...Confinguring hostapd...[+]"
-printf "country_code=us\ninterface=wlan0\nbridge=br0\nhw_mode=g\nchannel=7\nwmm_enabled=0\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n" | tee /etc/hostapd/hostapd.conf
-echo "ssid=***Your SSID***" | tee -a /etc/hostapd/hostapd.conf
-echo "wpa_passphrase=***Your Passphrase***" | tee -a /etc/hostapd/hostapd.conf
+echo "[+]...Configuring hostapd...[+]"
+printf "country_code=us\ninterface=wlan0\nbridge=br0\nhw_mode=g\nchannel=7\nwmm_enabled=0\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\nssid=$ssid\nwpa_passphrase=$passphrase" | tee /etc/hostapd/hostapd.conf
 if $? != 0;
     then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
     else echo "[+]***Command successful. Moving on***[+]" && sleep 3
 fin
 
-echo "[+]...Showing the sudo system where hostapg confing is...[+]"
+echo "[+]...Telling the system where hostapd config is...[+]"
 printf "DAEMON_CONF="/etc/hostapd/hostapd.conf"" | tee -a /etc/default/hostapd
 if $? != 0;
     then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
@@ -65,7 +66,7 @@ if $? != 0;
     else echo "[+]***Command successful. Moving on***[+]" && sleep 3
 fin
 
-echo "[+]...Adding a new bridge...[+]"
+echo "[+]...Adding the bridge interface as br0...[+]"
 brctl addbr br0
 if $? != 0;
     then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
@@ -80,7 +81,7 @@ if $? != 0;
 fin
 
 echo "[+]...Adding br0 to Network Interfaces...[+]"
-printf "auto br0 \niface br0 inet manual \nbridge_ports eth0 wlan0"
+printf "auto br0\niface br0 inet manual\nbridge_ports eth0 wlan0"
 if $? != 0;
     then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
     else echo "[+]***Command successful. Moving on***[+]" && sleep 3
