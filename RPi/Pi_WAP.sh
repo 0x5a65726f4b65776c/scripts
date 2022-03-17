a01cd915aaae03701c25c0235763868c72641f2f
0x5a65726f4b65776c <nicholas.ashley82@gmail.com>
2022-03-16 20:01:44 -0400

    Update Pi_WAP.sh

--- RPi/Pi_WAP.sh
+++ RPi/Pi_WAP.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
 #This will download and configure everything needed to turn your Pi into a Wireless Access Point
+read -p 'Network name: ' ssid
+read -p 'Network passphrase: ' passphrase
 
 {
 echo "Logging script errors to $PWD/Pi_WAP_Error_Log.txt"
-
-read -p 'Network name: ' ssid
-read -p 'Network passphrase: ' passphrase
 
 echo "[+]...Updating your system...[+]"
 sleep 3
@@ -35,70 +34,70 @@ echo "[+]...Configuring the DHCP server...[+]"
 sleep 1
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 printf "interface=wlan0\n dhcp-range=192.168.0.2,192.168.0.254,255.255.255.0,24h\n" | tee -a /etc/dnsmasq.conf
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command Failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+		else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Configuring hostapd...[+]"
-printf "country_code=us\ninterface=wlan0\nbridge=br0\nhw_mode=g\nchannel=7\nwmm_enabled=0\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\nssid=$ssid\nwpa_passphrase=$passphrase" | tee /etc/hostapd/hostapd.conf
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+printf "country_code=US\ninterface=wlan0\nbridge=br0\nhw_mode=g\nchannel=7\nwmm_enabled=0\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\nssid=$ssid\nwpa_passphrase=$passphrase" | tee /etc/hostapd/hostapd.conf
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+    	else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Telling the system where hostapd config is...[+]"
 printf "DAEMON_CONF="/etc/hostapd/hostapd.conf"" | tee -a /etc/default/hostapd
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+    	else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Forwarding IP traffic...[+]"
 printf "net.ipv4.ip_forward=1" | tee -a /etc/sudo sysctl.conf
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+    	else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Editing IPTables rules...[+]"
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sh -c "iptables-save > /etc/iptables.ipv4.nat"
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+    	else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Adding the bridge interface as br0...[+]"
 brctl addbr br0
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+    	else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Linking br0 to eth0...[+]"
 brctl addif br0 eth0
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+		else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Adding br0 to Network Interfaces...[+]"
 printf "auto br0\niface br0 inet manual\nbridge_ports eth0 wlan0"
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+		else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Enabling services...[+]"
 sudo systemctl enable tor
 sudo systemctl enable privoxy
 sudo systemctl enable hostapd
 sudo systemctl enable dnsmasq
-if $? !=0;
-then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
-    else echo "[+]***Command successful. Moving on***[+]" && sleep 3
-fin
+if $? != 0;
+	then echo "[+]***Command failed. Exiting now***[+]" && cat ./Pi_WAP_Error_Log.txt
+		else echo "[+]***Command successful. Moving on***[+]" && sleep 3
+fi
 
 echo "[+]...Rebooting now...[+]"
 sleep 3
